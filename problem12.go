4b4e73f61ea30fab7c0adbfb0a27d68383a7bb3c
Lake <109396050+lastCoyotes@users.noreply.github.com>
2022-08-01 00:20:33 -0500

    Create problem12.go
    slow. should use faster methods of finding prime factors (not checking the entire range either even)

---
+++ problem12.go
@@ -0,0 +1,27 @@
+package main
+
+// takes int, returns triangle number
+func nthTriangleNumber(x int)(ret int){
+	var triangle	int
+	triangle = (x*(x+1))/2
+	return triangle
+}
+
+// takes int, returns list of its divisors
+func listDivisors(x int)(ret []int){
+	var list	[]int
+
+	for i:=1; i<=x; i++{
+		if x%i == 0{
+			list = append(list, i)
+		}
+	}
+	return list
+}
+
+func main() {
+
+	for n:=0; len(listDivisors(nthTriangleNumber(n))) < 500; n++{
+		println(nthTriangleNumber(n))
+	}
+}
